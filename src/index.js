754dd424f62b12d89b3594c816d84585b51095eb
braincountpossible <melisa.4200048@gmail.com>
2025-08-02 02:32:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import SozDefteri from "./SozDefteri";
+import ReactDOM from "react-dom/client";
+import SozDefteri from "./SozDefteri.jsx";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <SozDefteri />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SozDefteri />);
