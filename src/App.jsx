da70af3f555d0d434ef7e624f3a00f4866928e71
Melisa <melisa.4200048@gmail.com>
2025-08-02 19:20:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Menu from "./Menu";
 import SozDefteri from "./SozDefteri";
+import History from "./History";
+import Gallery from "./Gallery";
+import Wishes from "./Wishes";
+import Advice from "./Advice";
 
 export default function App() {
   return (
@@ -9,9 +13,11 @@ export default function App() {
       <Menu />
       <Switch>
         <Route exact path="/" component={SozDefteri} />
-        {/* Можеш да добавиш още Route-и тук за други страници */}
+        <Route path="/history" component={History} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/wishes" component={Wishes} />
+        <Route path="/advice" component={Advice} />
       </Switch>
     </Router>
   );
 }
-
