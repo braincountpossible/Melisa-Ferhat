34dc8542036f5fd18ee691b38804428ac97a02bb
Melisa <melisa.4200048@gmail.com>
2025-08-02 17:04:06 +0300

    Create Advice.jsx

---
+++ src/pages/Advice.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Advice() {
+  return (
+    <div style={{ padding: 48 }}>
+      <h2>Съвети към младото семейство</h2>
+      <p>Тук ще бъдат събрани съвети и напътствия за щастлив семеен живот.</p>
+    </div>
+  );
+}
