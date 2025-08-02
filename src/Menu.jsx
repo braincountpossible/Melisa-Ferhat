c111258d5539f8ec589df7dc45db00a210913755
Melisa <melisa.4200048@gmail.com>
2025-08-02 19:19:27 +0300

    Update Menu.jsx

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -11,10 +11,10 @@ export default function Menu() {
       fontFamily: "serif"
     }}>
       <Link to="/" style={{ color: "#fff", textDecoration: "none", fontWeight: "bold" }}>Начало</Link>
-      {/* <Link to="/history" style={{ color: "#fff", textDecoration: "none" }}>Нашата история</Link>
+      <Link to="/history" style={{ color: "#fff", textDecoration: "none" }}>Нашата история</Link>
       <Link to="/gallery" style={{ color: "#fff", textDecoration: "none" }}>Галерия</Link>
       <Link to="/wishes" style={{ color: "#fff", textDecoration: "none" }}>Пожелания</Link>
-      <Link to="/advice" style={{ color: "#fff", textDecoration: "none" }}>Съвети</Link> */}
+      <Link to="/advice" style={{ color: "#fff", textDecoration: "none" }}>Съвети</Link>
     </nav>
   );
 }
