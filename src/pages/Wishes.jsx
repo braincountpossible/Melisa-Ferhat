f2d4634d1a575ecf8472059d63a9433348ebc038
Melisa <melisa.4200048@gmail.com>
2025-08-02 17:03:28 +0300

    Create Wishes.jsx

---
+++ src/pages/Wishes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Wishes() {
+  return (
+    <div style={{ padding: 48 }}>
+      <h2>Пожелания</h2>
+      <p>Гостите ще могат да оставят своите пожелания тук.</p>
+    </div>
+  );
+}
