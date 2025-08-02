5b66491bd6da4c5c663534aad520f01d1cd02054
Melisa <melisa.4200048@gmail.com>
2025-08-02 19:18:20 +0300

    Create Gallery.jsx

---
+++ src/Gallery.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Gallery() {
+  return (
+    <div style={{ padding: 48 }}>
+      <h2>Галерия</h2>
+      <p>Тук ще се показват снимките от събитието.</p>
+    </div>
+  );
+}
