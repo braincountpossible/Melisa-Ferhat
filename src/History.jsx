dd7d3b7b6b2c9aae635f21cfa59f902c164dbf1d
Melisa <melisa.4200048@gmail.com>
2025-08-02 19:14:21 +0300

    Create History.jsx

---
+++ src/History.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function History() {
+  return (
+    <div style={{ padding: 48 }}>
+      <h2>Нашата история</h2>
+      <p>Тук ще разкажем нашата любовна история... (попълни по желание)</p>
+    </div>
+  );
+}
