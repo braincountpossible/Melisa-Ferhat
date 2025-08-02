6bfc9436939c8cf92f8d9d169744df12c7a84921
Melisa <melisa.4200048@gmail.com>
2025-08-02 17:02:31 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 48 }}>
+      <h2>Нашата история</h2>
+      <p>Тук ще опишем историята на Мелиса и Ферхат. Място за романтични разкази, снимки и спомени...</p>
+    </div>
+  );
+}
