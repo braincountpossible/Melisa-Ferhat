474678c7648df548dd97ec577a3fd074b82e996a
Melisa <melisa.4200048@gmail.com>
2025-08-02 16:59:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SozDefteri from "./SozDefteri.jsx";
+import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<SozDefteri />);
+root.render(<App />);
